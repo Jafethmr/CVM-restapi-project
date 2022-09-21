ab918b896c5f55145cb095d43c774f3d19f968e9
Jafeth Morales <jafeth.morales@summa-consulting.net>
2022-09-21 15:42:54 -0500

    Correccion puerto DB

--- src/db.js
+++ src/db.js
@@ -4,13 +4,13 @@ import {
   DB_HOST,
   DB_PASSWORD,
   DB_USER,
-  DB_PORT,
+  DB_HOST_PORT,
 } from "./config.js";
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  port: DB_PORT,
+  port: DB_HOST_PORT,
   database: DB_DATABASE,
 });
